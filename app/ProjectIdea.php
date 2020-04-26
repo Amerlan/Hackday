d612860111f366ade55704613b1c0d4877a2d41a
Amerlan <36895439+Amerlan@users.noreply.github.com>
2020-04-27 02:25:22 +0600

    base get api
    get club api
    creating models
    creating club seeder

---
+++ app/ProjectIdea.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectIdea extends Model
+{
+    //
+}
---
+++ database/migrations/2020_04_26_153327_create_clubs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clubs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('members');
+            $table->string('leader');
+            $table->string('subleader');
+            $table->string('occupation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clubs');
+    }
+}
